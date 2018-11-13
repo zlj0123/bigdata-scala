a0fe2524bc66e597a9f2ea926fa2f884909c0ba6
zlj0123 <zlj0123@gmail.com>
2018-11-13 10:52:39 +0800

    add .gitignore file

--- src/main/scala/io/ibigdata/scala/test/Hello.scala
+++ src/main/scala/io/ibigdata/scala/test/Hello.scala
@@ -3,5 +3,10 @@ package io.ibigdata.scala.test
 class Hello {
   def sayHello(name:String): Unit = {
     println(name)
+    println(f"Hello $name")
+
+    var n = 5
+    val x = {n -= 1}
+    println(x)
   }
 }
